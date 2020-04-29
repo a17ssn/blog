15b8120e74b2d46e998bd480421e3f2c61f0db31
alhassn alshreaf <43709748+afsealshreaf@users.noreply.github.com>
2020-04-30 00:32:35 +0200

    save

--- bg/models.py
+++ bg/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
+from django.utils.text import slugify
 # Create your models here.
+
+class Notes(models.Model):
+  user    =  models.ForeignKey(User, on_delete=models.CASCADE)
+  title   = models.CharField(max_length=50)
+  slug    = models.SlugField(null=True , blank=True)
+  content = models.TextField(blank=True)
+  created = models.DateTimeField(blank=True, auto_now=True , auto_now_add=True)
+  active  = models.BooleanField(default=True)
+  tages   = models.CharField(blank=True, max_length=100)
+  
+  def save(self , *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify(self.title)
+    super(Notes , self).save(*args, **kwargs)
+
+
+  def __str__(self):
+      return self.title
+  
+
+
+
+
+
+
+
+
